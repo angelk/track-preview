8b07d73da0498dab60869c2ee94eee8dc84829e3
po_taka <angel.koilov@gmail.com>
2020-12-28 19:24:42 +0200

    Bresenham's line algorithm

--- main.go
+++ main.go
@@ -8,26 +8,29 @@ import (
 	"image/png"
 	"log"
 	"os"
+	"math"
 )
 
 func main() {
-	fmt.Println("test")
-	myImage := image.NewRGBA(image.Rect(0, 0, 10, 20))
+	imageX := 200
+	imageY := 200
+	myImage := image.NewRGBA(image.Rect(0, 0, imageX, imageY))
 
 	red := color.RGBA{255, 0, 0, 255}
 	blue := color.RGBA{0, 0, 255, 255}
+	white := color.RGBA{255, 255, 255, 255}
+
+	rectangle := image.Rect(0, 0, imageX, imageY)
+	draw.Draw(myImage, rectangle, &image.Uniform{white}, image.ZP, draw.Src)
 
 	fmt.Println(red)
 
-	myImage.Set(0, 0, red)
-	myImage.Set(5, 0, red)
-	myImage.Set(9, 0, red)
-	myImage.Set(2, 1, red)
-
-	rectangle := image.Rect(2, 2, 7, 15)
-	draw.Draw(myImage, rectangle, &image.Uniform{blue}, image.ZP, draw.Src)
-
+	drawLine(myImage, 0, 0, 50, 50, blue)
 	drawLine(myImage, 0, 0, 10, 20, red)
+	drawLine(myImage, 50, 50, 100, 0, red)
+
+	drawLine(myImage, 50, 50, 0, 10, blue)
+	drawLine(myImage, 0, 10, 50, 50, blue)
 
 	outputFile, err := os.Create("test.png")
 	if err != nil {
@@ -38,26 +41,37 @@ func main() {
 	outputFile.Close()
 }
 
-// buggy
+// draw line between two points using Bresenham's line algorithm
 func drawLine(i *image.RGBA, x, y, x1, y1 int, c color.RGBA) {
 	if x == x1 && y == y1 {
 		return
 	}
 
-	newPoint := x
-	if x > x1 {
-		newPoint = x - 1
-	} else if x < x1 {
-		newPoint = x + 1
+	if (x > x1) {
+		x, x1 = x1, x
+		y, y1 = y1, y
 	}
 
-	newY := y
-	if y > y1 {
-		newY = y - 1
-	} else if y < y1 {
-		newY = y + 1
-	}
+	deltaX := x1 - x
+	deltaY := y1 - y
 
-	i.Set(newPoint, newY, c)
-	drawLine(i, newPoint, newY, x1, y1, c)
+	deltaErr := math.Abs(float64(deltaY) / float64(deltaX))
+
+	var realError float64 = 0
+
+	tmpY := float64(y)
+
+	for tmpX := x; tmpX <= x1; tmpX++ {
+		i.Set(tmpX, int(tmpY), c)
+
+		realError += deltaErr
+		if realError >= 0.5 {
+			if (y1 > y) {
+				tmpY += deltaErr
+			} else {
+				tmpY -= deltaErr
+			}
+			realError -= 1
+		}
+	}
 }
